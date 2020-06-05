d3547fd227937687e9eb65ad9b33cdee68589cb5
Gillett Hernandez <gillett.hernandez@gmail.com>
2020-06-04 19:24:30 -0700

    update to support topleft and size parameters

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,22 @@ extern crate num_complex;
 use rand::prelude::*;
 
 fn main() {
-    let imgx = 2048;
+    let imgx = (2048.0 * 1.5) as u32;
     let imgy = 2048;
 
-    let scalex = 1.0 / imgx as f32;
-    let scaley = 1.0 / imgy as f32;
-    let scale: f32 = 3.0;
-    let aa_steps = 3;
+    let aspect_ratio: f64 = (imgx as f64) / (imgy as f64);
+
+    let aa_steps = 4;
+    let mut topleft = (-2.0, -1.0);
+    let mut size: f64 = 2.0;
+    let (pixel_width, pixel_height) = ((aspect_ratio * size) / imgx as f64, size / imgy as f64);
 
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
     // Iterate over the coordinates and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (128.0 * scalex * x as f32) as u8;
-        let b = (128.0 * scaley * y as f32) as u8;
+        let (r, b) = (0, 0);
         *pixel = image::Rgb([r, 0, b]);
     }
 
@@ -27,9 +28,9 @@ fn main() {
     let ix = 0;
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let mut i = 0;
-        for aai in 0..aa_steps {
-            let cx = ((x as f32 + random::<f32>()) * scalex - 0.7) * scale;
-            let cy = ((y as f32 + random::<f32>()) * scaley - 0.5) * scale;
+        for _ in 0..aa_steps {
+            let cx = ((x as f64 + random::<f64>()) * pixel_width + topleft.0);
+            let cy = ((y as f64 + random::<f64>()) * pixel_height + topleft.1);
 
             let c = num_complex::Complex::new(cx, cy);
             let mut z = num_complex::Complex::new(0.0, 0.0);
@@ -43,7 +44,7 @@ fn main() {
         }
 
         let image::Rgb(data) = *pixel;
-        *pixel = image::Rgb([data[0], (i as f32 / aa_steps as f32) as u8, data[2]]);
+        *pixel = image::Rgb([data[0], (i as f64 / aa_steps as f64) as u8, data[2]]);
     }
 
     // Save the image as “fractal.png”, the format is deduced from the path
