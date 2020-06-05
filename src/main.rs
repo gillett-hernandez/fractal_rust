9840a4ddb97f15b8f03dddd34faac826be4653a0
Gillett Hernandez <gillett.hernandez@gmail.com>
2020-06-04 21:06:57 -0700

    better live updates, still not the best

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,24 @@ use std::time::Duration;
 fn mandelbrot_set() {}
 
 fn main() {
-    let imgx = (2048.0 * 1.0) as u32;
-    let imgy = 2048;
+    const WIDTH: usize = 1800;
+    const HEIGHT: usize = 900;
+    // let imgx = (1920.0 * 1.0) as u32;
+    let imgx = WIDTH as u32;
+    // let imgy = 1080 as u32;
+    let imgy = HEIGHT as u32;
 
     let aspect_ratio: f64 = (imgx as f64) / (imgy as f64);
 
     let aa_steps = 1;
-    // let mut topleft = (-2.0, -1.0);
-    // let mut size: f64 = 2.0;
-    let mut topleft = (-0.1011, 0.9563);
-    let mut size: f64 = 0.01;
+    let mut topleft = (-2.0, -1.0);
+    let mut size: f64 = 2.0;
+    // let mut topleft = (-0.1011, 0.9563);
+    // let mut size: f64 = 0.01;
 
     let (pixel_width, pixel_height) = ((aspect_ratio * size) / imgx as f64, size / imgy as f64);
 
     ///////////////////////////////
-
-    const WIDTH: usize = 1920;
-    const HEIGHT: usize = 1080;
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
@@ -46,11 +47,13 @@ fn main() {
     });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(std::time::Duration::from_micros(6944)));
 
+    let mut pixel_i = 0;
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        let mut pixel_i = 0;
-        for (pixel) in buffer.iter_mut() {
+        // for (pixel) in buffer.iter_mut() {
+        for _ in 0..(WIDTH) {
+            let mut pixel = &mut buffer[pixel_i];
             let mut i = 0;
             let (x, y) = (pixel_i % WIDTH, pixel_i / WIDTH);
             for _ in 0..aa_steps {
@@ -69,12 +72,13 @@ fn main() {
             }
 
             pixel_i += 1;
+            pixel_i = pixel_i % (WIDTH * HEIGHT);
 
             // let image::Rgb(data) = *pixel;
             // *pixel = image::Rgb([data[0], (i as f64 / aa_steps as f64) as u8, data[2]]);
-            *pixel = ((i as f64 / aa_steps as f64) as u32) << 8;
+            *pixel = ((i as f64 / aa_steps as f64) as u32) << 16;
+            // }
         }
-
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
